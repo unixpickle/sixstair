1eeb1b0c004e2968d76ecd1dedfb0ae2472b4771
Alex Nichol <unixpickle@gmail.com>
2014-08-03 20:32:32 -0400

    moved Dart solver

--- SixStairSolver/SixStairSolver/main.cpp
+++ SixStairSolver/SixStairSolver/main.cpp
@@ -8,15 +8,15 @@ using std::cerr;
 using std::cin;
 using std::endl;
 
-bool SolveAmount(State & s, int & numSolved, int maxDepth);
+bool SolveAmount(State & s, int & numSolved, int maxDepth, bool upwards);
 
 int main(int argc, const char * argv[]) {
   if (argc > 2) {
-    cerr << "Usage: SixStairSolver [maxDepth (10)]" << endl;
+    cerr << "Usage: SixStairSolver [maxDepth (9)]" << endl;
     return 1;
   }
   
-  int maxDepth = 10;
+  int maxDepth = 9;
   if (argc == 2) {
     maxDepth = atoi(argv[1]);
   }
@@ -32,7 +32,8 @@ int main(int argc, const char * argv[]) {
   int numSolved = 0;
   int tempMaxDepth = maxDepth;
   while (numSolved < 21) {
-    if (!SolveAmount(state, numSolved, tempMaxDepth)) {
+    bool upwards = tempMaxDepth != maxDepth;
+    if (!SolveAmount(state, numSolved, tempMaxDepth, upwards)) {
       tempMaxDepth++;
     } else {
       tempMaxDepth = maxDepth;
@@ -42,8 +43,16 @@ int main(int argc, const char * argv[]) {
   return 0;
 }
 
-bool SolveAmount(State & s, int & numSolved, int maxDepth) {
-  for (int amount = 21; amount > numSolved; --amount) {
+bool SolveAmount(State & s, int & numSolved, int maxDepth, bool upwards) {
+  int step, start;
+  if (upwards) {
+    step = 1;
+    start = numSolved + 1;
+  } else {
+    step = -1;
+    start = 21;
+  }
+  for (int amount = start; amount > numSolved && amount < 22; amount += step) {
     for (int i = 0; i <= maxDepth; ++i) {
       LinearSolver solver(s, i, amount);
       if (solver.Solve()) {
